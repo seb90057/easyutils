0a36bd8fca643d2fec3cf7d07bb8ec65b733a63a
seb90057 <seb.delarue@gmail.com>
2020-05-19 01:00:35 +0200

    change name

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("requirements/common.txt", "r") as handler:
 
 setuptools.setup(
     install_requires=install_requirements,
-    name="easyutils",
+    name="sdeutils",
     version="0.0.3",
     author="SDE",
     description="various utils",
     package_dir={"": "src"},
     packages=setuptools.find_packages("src"),
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
