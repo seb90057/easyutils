c630dacaa89a75f185ece923dca2bdaeaf77b6ac
seb90057 <seb.delarue@gmail.com>
2020-05-19 04:42:19 +0200

    adapte according newly field class

--- tests/sdeutils/file/field/test_field.py
+++ tests/sdeutils/file/field/test_field.py
@@ -1,24 +1,15 @@
 import unittest
-from sdeutils.file.field.cast.cast import DATE, INTEGER, FLOAT
+from sdeutils.file.field.cast.cast import DATE
 from sdeutils.file.field.field import Field
+import datetime
 
 
 class TestField(unittest.TestCase):
     def setUp(self) -> None:
-        self.float_value_list = ["20200518", "0000.05", "10. 30", "4", "37"]
-        self.float_field = Field(self.float_value_list)
+        self.date_value = "27/01/1983"
+        self.date_field = Field(self.date_value)
 
     def test_field(self):
-
-        expect_cst = {
-            DATE: {"count": 1, "values": ["20200518"]},
-            FLOAT: {"count": 2, "values": ["0000.05", "10. 30"]},
-            INTEGER: {"count": 2, "values": ["4", "37"]},
-        }
-
-        self.assertDictEqual(self.float_field.cast_estimation_dict, expect_cst)
-        self.assertTrue(self.float_field.estimated_cast == FLOAT)
-        self.assertTrue(self.float_field.discrepancy)
-
-    def test_get_cast(self):
-        self.assertTrue(self.float_field.get_cast() == FLOAT)
+        self.assertTrue(self.date_field.type == DATE)
+        exp_dt = datetime.datetime(1983, 1, 27)
+        self.assertTrue(self.date_field.refined_value == exp_dt)
