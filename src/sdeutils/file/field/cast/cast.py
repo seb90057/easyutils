3ce5a27cbcbc325ba6db13a04076eee5ca4d02a1
seb90057 <seb.delarue@gmail.com>
2020-05-23 06:39:26 +0200

    get cast more unit manager

--- src/sdeutils/file/field/cast/cast.py
+++ src/sdeutils/file/field/cast/cast.py
@@ -18,75 +18,121 @@ NONE_PATH = "/conf/none.json"
 STRING_PATH = "/conf/string.json"
 
 
-priority = {
-    FLOAT: [INTEGER, NONE],
-    INTEGER: [DATE, BOOLEAN, NONE],
-    STRING: [INTEGER, FLOAT, INTEGER, BOOLEAN, NONE],
-    DATE: [NONE],
-    NONE: [NONE],
-    BOOLEAN: [NONE],
-}
-
-
 class Cast:
-    patterns = {
+    loaded = False
+
+    def __init__(self, value, cast_type):
+        if not Cast.loaded:
+            Cast.load_patterns()
+
+        self.cast_type = cast_type
+        self.value = value
+        self.arg = None
+        self.casted_value = None
+        self.pattern = None
+        self.pattern_id = None
+        self.comment = None
+        self.is_casted = False
+        self.cast()
+
+    def is_(self, tpe):
+        elt = str(self.value)
+        if tpe in [INTEGER, FLOAT]:
+            elt = elt.replace(" ", "")
+
+        if tpe in [BOOLEAN]:
+            elt = elt.lower()
+
+        pattern_list = Cast.cast_details[tpe]["pattern_list"]
+        for pattern in pattern_list:
+            if pattern["pattern"].match(elt) is not None:
+                return pattern
+        return None
+
+    def to_date(self):
+        return datetime.datetime.strptime(self.value, self.arg)
+
+    def to_integer(self):
+        clean_value = float(str(self.value).replace(" ", ""))
+        return int(clean_value)
+
+    def to_float(self):
+        if self.pattern == 2:
+            m = self.pattern.search(self.value)
+            clean_value = float(m.group("num")) ** float(-m.group("exp"))
+        else:
+            clean_value = str(self.value).replace(" ", "").replace(",", ".")
+        return float(clean_value)
+
+    def to_boolean(self):
+        if str(self.value).lower() in ["true", "vrai", "1"]:
+            return True
+        elif str(self.value).lower() in ["false", "faux", "0"]:
+            return False
+        else:
+            raise Exception
+
+    def to_string(self):
+        return str(self.value)
+
+    def cast(self):
+        res = self.is_(self.cast_type)
+        if res:
+            self.is_casted = True
+            self.pattern = res["pattern"]
+            self.comment = res["comment"]
+            self.pattern_id = res.get("pattern_id")
+            self.arg = res.get("arg")
+            self.casted_value = Cast.to_(self.cast_type, self)
+
+    cast_details = {
         DATE: {
             "pattern_list": None,
             "path": pkg_resources.resource_filename(__name__, DATE_PATH),
+            "method": to_date,
+            "priority": 1,
         },
         INTEGER: {
             "pattern_list": None,
             "path": pkg_resources.resource_filename(__name__, INTEGER_PATH),
+            "method": to_integer,
+            "priority": 3,
         },
         FLOAT: {
             "pattern_list": None,
             "path": pkg_resources.resource_filename(__name__, FLOAT_PATH),
+            "method": to_float,
+            "priority": 4,
         },
         BOOLEAN: {
             "pattern_list": None,
             "path": pkg_resources.resource_filename(__name__, BOOLEAN_PATH),
-        },
-        NONE: {
-            "pattern_list": None,
-            "path": pkg_resources.resource_filename(__name__, NONE_PATH),
+            "method": to_boolean,
+            "priority": 2,
         },
         STRING: {
             "pattern_list": None,
             "path": pkg_resources.resource_filename(__name__, STRING_PATH),
+            "method": to_string,
+            "priority": 5,
         },
     }
 
-    cast_list_ordered = [DATE, INTEGER, FLOAT, BOOLEAN, NONE, STRING]
+    @staticmethod
+    def to_(cast_type, cst):
+        cast_method = Cast.cast_details[cast_type]["method"]
+        try:
+            return cast_method(cst)
+        except Exception as e:
+            print(e)
+            return None
 
-    def __init__(self, value):
-        self.value = value
-        self.type = None
-        self.pattern = None
-        self.comment = None
-        self.method = None
-        self.arg = None
-        for ct in Cast.patterns:
-            path = Cast.patterns[ct]["path"]
-            Cast.patterns[ct]["pattern_list"] = Cast.load_pattern(path)
-        self.cast()
-        self.refined_value = getattr(Cast, self.method)(self.value, self.arg)
-
-    def cast(self):
-        for t in Cast.cast_list_ordered:
-            res = Cast.is_(self.value, t)
-            if res:
-                self.type = t
-                self.pattern = res["pattern"]
-                self.comment = res["comment"]
-                self.method = res["method"]
-                self.arg = res.get("arg")
-                break
-
-    def __str__(self):
-        res = []
-        for k, v in self.__dict__.items():
-            res.append("{}: {}".format(k, v))
-        return "\n".join(res)
+    @staticmethod
+    def load_patterns():
+        for ct in Cast.cast_details:
+            path = Cast.cast_details[ct]["path"]
+            Cast.cast_details[ct]["pattern_list"] = Cast.load_pattern(path)
+        Cast.loaded = True
 
     @staticmethod
     def load_pattern(path):
@@ -107,72 +153,18 @@ class Cast:
         return pattern_list
 
     @staticmethod
-    def is_(elt, tpe):
-        elt = str(elt)
-        if tpe in [INTEGER, FLOAT]:
-            elt = elt.replace(" ", "")
-
-        if tpe in [BOOLEAN]:
-            elt = elt.lower()
-
-        pattern_list = Cast.patterns[tpe]["pattern_list"]
-        for pattern in pattern_list:
-            if pattern["pattern"].match(elt) is not None:
-                return pattern
-        return None
-
-    @staticmethod
-    def overload_cast(cast_list):
-        res = list(set(cast_list))
-
-        for c in res:
-            weak_elts = priority[c]
-            res = [elt if elt not in weak_elts else c for elt in res]
-
-        res = list(set(res))
-        return res[0]
-
-    @staticmethod
-    def cast_list(casts):
-        res = {}
-        for cst in casts:
-            ct = cst.type
-            if ct in res:
-                res[ct] += 1
-            else:
-                res[ct] = 1
+    def get_ordered_cast_type():
+        res = [
+            k
+            for k, v in sorted(
+                Cast.cast_details.items(), key=lambda item: item[1]["priority"]
+            )
+        ]
         return res
-
-    @staticmethod
-    def to_date(value, tr):
-        return datetime.datetime.strptime(value, tr)
-
-    @staticmethod
-    def to_integer(value, tr):
-        clean_value = float(str(value).replace(" ", ""))
-        return int(clean_value)
-
-    @staticmethod
-    def to_float(value, tr):
-        clean_value = str(value).replace(" ", "")
-        return float(clean_value)
-
-    @staticmethod
-    def to_boolean(value, tr):
-        if str(value).lower() in ["true", "vrai", "1"]:
-            return True
-        elif str(value).lower() in ["false", "faux", "0"]:
-            return False
-        else:
-            raise Exception
-
-    @staticmethod
-    def to_string(value, tr):
-        return str(value)
 
 
 if __name__ == "__main__":
+    Cast.load_patterns()
+    c = Cast("1.3", FLOAT)
 
-    c = Cast("1")
-
-    print(c)
+    print(c.casted_value)
